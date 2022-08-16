99ff5278f0b0ba35d167099295919d1f36b54954
c-o-d-e <swapyuk@gmail.com>
2022-08-16 22:01:46 +0100

    addOne subtractOne methods

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
-import logo from "./logo.svg";
+import { Component } from "react";
 import "./App.css";
+import React from "react";
 
-function App() {
-    return (
-        <div className="App">
-            <main>
-                <div id="button-wrapper">
-                    <button class="buttons">+</button>
-                    <button class="buttons">-</button>
-                </div>
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0,
+        };
+    }
 
-                <h1>0</h1>
-            </main>
-        </div>
-    );
+    addOne() {
+        let counters = this.state.counter;
+        this.setState({ counter: (counters += 1) });
+    }
+
+    subtractOne() {
+        let counters = this.state.counter;
+        this.setState({ counter: (counters -= 1) });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <main>
+                    <h1>Simple Counter</h1>
+                    <div id="button-wrapper">
+                        <button
+                            className="buttons"
+                            onClick={() => this.addOne()}
+                        >
+                            +
+                        </button>
+                        <button
+                            className="buttons"
+                            onClick={() => this.subtractOne()}
+                        >
+                            -
+                        </button>
+                    </div>
+                    <h1>{this.state.counter}</h1>
+                </main>
+            </div>
+        );
+    }
 }
 
 export default App;
